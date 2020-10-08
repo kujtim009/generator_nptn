9b00761705bec19c18b1f55872a958450b8cfb00
kujtim009 <kujtim009@gmail.com>
2020-10-08 02:05:27 +0200

    minor ajustment

--- neptunGrabber.py
+++ neptunGrabber.py
@@ -15,8 +15,9 @@ def getCategories(mainUrl):
 
 
 def appendToFile(content):
-    with open("sample2.txt", "a+") as file_object:
-        file_object.write(content)
+    strContent = " "
+    with open("data.txt", "a") as f:
+        f.write(strContent.join(content))
 
 
 def getPages(link):
@@ -38,11 +39,6 @@ def getDipLinks(link):
         r'Url\\"\:\\"(.*?)\".*?Url\\"\:\\"(.*?)\"', req.text, flags=re.I | re.DOTALL)
     return list("{}pocetna/categories/{}{}".format(mainUrl, n[0].replace("\\", "/"), n[1].replace("\\", "")) for n in reg)
 
-# def thread_function(name):
-#     print("HELLO {}", format(name))
-
-# url = ["url1", "url2", "url3"]
-
 
 def test():
     pass
@@ -51,5 +47,6 @@ def test():
 if __name__ == "__main__":
     print("starting")
     categorit = getCategories(mainUrl)
-    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+    print(len(categorit))
+    with concurrent.futures.ThreadPoolExecutor(max_workers=13) as executor:
         executor.map(getPages, [link for link in categorit])
