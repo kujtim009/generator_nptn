b10077b78ff9e834e4e0e0717a42c9e887e80a91
Kujtim Durguti <kujtim009@gmail.com>
2020-10-09 13:18:01 +0200

    test

--- neptunGrabber.py
+++ neptunGrabber.py
@@ -5,10 +5,20 @@ import re
 
 mainUrl = "https://www.neptun-ks.com/"
 deepLinksList = []
+useProxy = True
+
+
+def proxies():
+    return {
+        "http://": "192.187.111.82:17086",
+        "https://": "192.187.111.82:17086"
+    }
 
 
 def getCategories(mainUrl):
-    req = requests.get(mainUrl)
+    req = (requests.get(
+        mainUrl, proxies=proxies()) if useProxy else requests.get(mainUrl))
+
     reg = re.findall(
         r"<li id=\"\"><a href=\"/pocetna/categories(.*?)\" target=\"_self\">", req.text)
     return list("{}pocetna/categories{}".format(mainUrl, n) for n in reg)
@@ -16,8 +26,10 @@ def getCategories(mainUrl):
 
 def appendToFile(content):
     strContent = " "
+    print("Content Saving...")
     with open("data.txt", "a") as f:
         f.write(strContent.join(content))
+        print("Content Saved")
 
 
 def getPages(link):
@@ -33,20 +45,17 @@ def getPages(link):
 
 
 def getDipLinks(link):
-    req = requests.get("{}&items=100".format(link))
-
+    req = requests.get("{}&items=100".format(
+        link), proxies=proxies) if useProxy else requests.get("{}&items=100".format(link))
+    print("Deep Links scraped")
     reg = re.findall(
         r'Url\\"\:\\"(.*?)\".*?Url\\"\:\\"(.*?)\"', req.text, flags=re.I | re.DOTALL)
     return list("{}pocetna/categories/{}{}".format(mainUrl, n[0].replace("\\", "/"), n[1].replace("\\", "")) for n in reg)
-
-
-def test():
-    pass
 
 
 if __name__ == "__main__":
     print("starting")
     categorit = getCategories(mainUrl)
     print(len(categorit))
-    with concurrent.futures.ThreadPoolExecutor(max_workers=13) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
         executor.map(getPages, [link for link in categorit])
